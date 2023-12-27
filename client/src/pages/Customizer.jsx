1366c54bc5b6f87c9f5a4d7255f5a070a51156fd
echo <play3step@naver.com>
2023-12-27 20:39:35 +0900

    feat: 3d canvas를 이용해 glb 파일 불러보기

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -19,38 +19,47 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
-  return <AnimatePresence>{!snap.intro &&
-   <>
-    <motion.div
-      key="custom"
-      className="absolute top-0 left-0 z-10"
-      {...slideAnimation('left')}
-    >
-      <div className="flex items-center min-h-screen">
-        <div className="editortabs-container tabs">
-          {EditorTabs.map((tab) =>(
-            <Tab
-            key={tab.name}
-            tab={tab}
-            handleClick={()=>{}}
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py2.5 font-bold text-sm"
             />
-          ))}
-        </div>
-      </div>
-    </motion.div>
-    <motion.div
-      className="absolute z-10 top-5 right-5"
-      {...fadeAnimation}
-    >
-      <CustomButton 
-        type="filled"
-        title="Go Back"
-        handleClick={() => state.intro = true}
-        customStyles="w-fit px-4 py2.5 font-bold text-sm"
-      />
-    </motion.div>
+          </motion.div>
 
-  </>}</AnimatePresence>;
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {EditorTabs.map((tab) => (
+              <Tab key={tab.name} tab={tab} isFilterTab isActiveTap="" handleClick={() => {}} />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
